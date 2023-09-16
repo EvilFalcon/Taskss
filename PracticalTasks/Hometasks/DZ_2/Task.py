d7c57ad206a386fb33c2b7c0b907c40b70d73406
EvilFalcon <112806793+EvilFalcon@users.noreply.github.com>
2023-09-16 22:54:24 +0300

    Added New Task

---
+++ PracticalTasks/Hometasks/DZ_2/Task.py
@@ -0,0 +1,64 @@
+import os
+
+
+def Main():
+    path = "Text/Result/"
+
+    settings = SetFormatting()
+    originalText = ReadLine()
+    formattedText = FormatingText(originalText, settings[0], settings[1], settings[2])
+    PrintTexts(originalText, formattedText)
+    SaveResult(formattedText, path)
+
+
+def SetFormatting():
+    replaceable = int(input("Ведите номербуквы (а) которая будет заменина : ")) - 1
+    replace = int(input("Ведите номер буквы (б) которая заменит букву (а)")) - 1
+    changeRegister = int(input("Ведите номер буквы (с) у которой будет изменён регистер")) - 1
+
+    return [replaceable, replace, changeRegister]
+
+
+def FormatingText(originalText, replaceable, replace, changeRegister):
+    words = originalText.split(" ")
+    text = ""
+    for word in words:
+        chars = list(word)
+        countChar = len(chars) - 1
+
+        if countChar >= replaceable and countChar >= replace:
+            chars[replaceable] = chars[replace]
+
+        if countChar >= changeRegister:
+
+            if word[changeRegister].isupper():
+                chars[changeRegister] = chars[changeRegister].lower()
+            elif word[changeRegister].islower():
+                chars[changeRegister] = chars[changeRegister].upper()
+
+        text = F'{text} {"".join(chars)}'
+
+    return text
+
+
+def PrintTexts(originalText, formattedText):
+    print("Оригинальный Текст {0} \n\nОтформатированный текст{1}".format(originalText, formattedText))
+
+
+def SaveResult(result, path):
+    fileName = "Result.txt"
+    resultFile = open(path + fileName, "w")
+    resultFile.write(result)
+    resultFile.close()
+
+
+def ReadLine():
+    text_file = open("Text/Text.txt")
+    text = text_file.read()
+    text_file.close()
+
+    return text
+
+
+if __name__ == '__main__':
+    Main()
