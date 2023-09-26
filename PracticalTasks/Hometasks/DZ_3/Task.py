c30bb7ef11fab22f969e92462d455105bc77e3c9
EvilFalcon <112806793+EvilFalcon@users.noreply.github.com>
2023-09-26 15:48:02 +0300

    update Task

--- PracticalTasks/Hometasks/DZ_3/Task.py
+++ PracticalTasks/Hometasks/DZ_3/Task.py
@@ -8,18 +8,28 @@ def Main():
 def GetNumber():
     return int(input("Введите число :"))
 
-def ConversionNumber(number):
-    ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
-    tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
-    hunds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
-    thous = ["", "M", "MM", "MMM", "MMMM"]
-
-    t = thous[number // 1000]
-    h = hunds[number // 100 % 10]
-    te = tens[number // 10 % 10]
-    o = ones[number % 10]
-
-    return t + h + te + o
+def ConversionNumber(number: int )-> str:
+    roman_numerals = {
+        1000: 'M',
+        900: 'CM',
+        500: 'D',
+        400: 'CD',
+        100: 'C',
+        90: 'XC',
+        50: 'L',
+        40: 'XL',
+        10: 'X',
+        9: 'IX',
+        5: 'V',
+        4: 'IV',
+        1: 'I'
+    }
+    roman_numeral = ''
+    for value, symbol in roman_numerals.items():
+        while number >= value:
+            roman_numeral += symbol
+            number -= value
+    return roman_numeral
 
 
 
